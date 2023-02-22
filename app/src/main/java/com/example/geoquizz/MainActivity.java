2e2351c795704acff2a9a18f6362821808457ca0
Alex Ciria Roca <alex.ciria@students.salle.url.edu>
2023-02-22 20:17:51 +0100

    Created string array in strings.xml and questions array

--- app/src/main/java/com/example/geoquizz/MainActivity.java
+++ app/src/main/java/com/example/geoquizz/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.geoquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Question> questions = new ArrayList<>();
+        boolean[] isTrue = {true, false, true, false, false, false, false, true, true, true};
+        Resources res = getResources();
+        String[] questionHeaders = res.getStringArray(R.array.questions);
+
+        for (int i=0; i<10; i++){
+
+            Question q = new Question();
+
+            q.setQuestion(questionHeaders[i]);
+            q.setTrue(isTrue[i]);
+
+            questions.add(q);
+        }
+
+
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
---
+++ app/src/main/java/com/example/geoquizz/Question.java
@@ -0,0 +1,25 @@
+package com.example.geoquizz;
+
+public class Question {
+
+    private String question;
+
+    private boolean isTrue;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public boolean getIsTrue() {
+        return isTrue;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setTrue(boolean aTrue) {
+        isTrue = aTrue;
+    }
+
+}
